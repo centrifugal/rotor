f62cbc124ae789b8843a10a4d64fa369e74df9d9
Michael Filonenko <filonenko.mikhail@gmail.com>
2021-02-12 20:35:30 +0300

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,29 @@
+#!/usr/bin/env tarantool
+
+--[[
+    Устаналвиваем текущую директорию,
+    как начальный путь загрузки всех модулей
+--]]
+package.setsearchroot()
+
+local cartridge = require('cartridge')
+local log = require('log')
+
+--[[
+    Конфигурируем и запускаем cartridge на узле
+    Указываем какие роли мы будем использовать в кластере
+    Указываем рабочую директорию для хранения снапов, икслогов
+    и конфигурации приложения `one`
+]]
+local _, err = cartridge.cfg({
+    workdir = 'one', -- default
+
+    roles = {
+        --<<< Добавляем роль - указываем путь к модулю
+        'centrifuge',
+    },
+})
+if err ~= nil then
+    log.info(err)
+    os.exit(1)
+end
