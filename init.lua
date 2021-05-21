a7198e0c91128b7e162df4ebec91e0a6c63ea5a4
Alexander Emelin <frvzmb@gmail.com>
2021-05-22 02:45:22 +0300

    format init files

--- init.lua
+++ init.lua
@@ -13,58 +13,66 @@ else
     -- root directory of that app, everything goes fine, but if you try to
     -- start your app with "tarantool myapp/init.lua", it will fail to load
     -- its modules, and modules from myapp/.rocks.
-    local fio = require('fio')
+    local fio = require("fio")
     local app_dir = fio.abspath(fio.dirname(arg[0]))
-    package.path = app_dir .. '/?.lua;' .. package.path
-    package.path = app_dir .. '/?/init.lua;' .. package.path
-    package.path = app_dir .. '/.rocks/share/tarantool/?.lua;' .. package.path
-    package.path = app_dir .. '/.rocks/share/tarantool/?/init.lua;' .. package.path
-    package.cpath = app_dir .. '/?.so;' .. package.cpath
-    package.cpath = app_dir .. '/?.dylib;' .. package.cpath
-    package.cpath = app_dir .. '/.rocks/lib/tarantool/?.so;' .. package.cpath
-    package.cpath = app_dir .. '/.rocks/lib/tarantool/?.dylib;' .. package.cpath
+    package.path = app_dir .. "/?.lua;" .. package.path
+    package.path = app_dir .. "/?/init.lua;" .. package.path
+    package.path = app_dir .. "/.rocks/share/tarantool/?.lua;" .. package.path
+    package.path = app_dir .. "/.rocks/share/tarantool/?/init.lua;" .. package.path
+    package.cpath = app_dir .. "/?.so;" .. package.cpath
+    package.cpath = app_dir .. "/?.dylib;" .. package.cpath
+    package.cpath = app_dir .. "/.rocks/lib/tarantool/?.so;" .. package.cpath
+    package.cpath = app_dir .. "/.rocks/lib/tarantool/?.dylib;" .. package.cpath
 end
 
-local log = require('log')
-local cartridge = require('cartridge')
-local argparse = require('cartridge.argparse')
-local membership = require('membership')
+local log = require("log")
+local cartridge = require("cartridge")
+local argparse = require("cartridge.argparse")
+local membership = require("membership")
 
---[[
-    Configure and run Cartridge on node.
-]]
-local _, err = cartridge.cfg({
-    workdir = 'tmp/',
-    roles = {
-        'centrifuge',
-    },
-})
+-- Configure and run Cartridge on node.
+local _, err =
+    cartridge.cfg(
+    {
+        workdir = "tmp/",
+        roles = {
+            "centrifuge"
+        }
+    }
+)
 if err ~= nil then
     log.info(err)
     os.exit(1)
 end
 
-local opts, err = argparse.get_opts({
-        bootstrap = 'boolean'})
+local opts, err =
+    argparse.get_opts(
+    {
+        bootstrap = "boolean"
+    }
+)
 
 if err ~= nil then
-    log.error('%s', tostring(err))
+    log.error("%s", tostring(err))
     os.exit(1)
 end
 
 if opts.bootstrap then
-    log.info('Bootstrapping in %s', workdir)
+    log.info("Bootstrapping in %s", workdir)
     require("membership.options").ACK_TIMEOUT_SECONDS = 0.5
     local all = {
-        ['centrifuge'] = true,
+        ["centrifuge"] = true
     }
 
-    local _, err = cartridge.admin_join_server({
+    local _, err =
+        cartridge.admin_join_server(
+        {
             uri = membership.myself().uri,
-            roles = all,
-    })
+            roles = all
+        }
+    )
 
     if err ~= nil then
-        log.warn('%s', tostring(err))
+        log.warn("%s", tostring(err))
     end
 end
--- init_standalone.lua
+++ init_standalone.lua
@@ -13,44 +13,44 @@ else
     -- root directory of that app, everything goes fine, but if you try to
     -- start your app with "tarantool myapp/init.lua", it will fail to load
     -- its modules, and modules from myapp/.rocks.
-    local fio = require('fio')
+    local fio = require("fio")
     local app_dir = fio.abspath(fio.dirname(arg[0]))
-    package.path = app_dir .. '/?.lua;' .. package.path
-    package.path = app_dir .. '/?/init.lua;' .. package.path
-    package.path = app_dir .. '/.rocks/share/tarantool/?.lua;' .. package.path
-    package.path = app_dir .. '/.rocks/share/tarantool/?/init.lua;' .. package.path
-    package.cpath = app_dir .. '/?.so;' .. package.cpath
-    package.cpath = app_dir .. '/?.dylib;' .. package.cpath
-    package.cpath = app_dir .. '/.rocks/lib/tarantool/?.so;' .. package.cpath
-    package.cpath = app_dir .. '/.rocks/lib/tarantool/?.dylib;' .. package.cpath
+    package.path = app_dir .. "/?.lua;" .. package.path
+    package.path = app_dir .. "/?/init.lua;" .. package.path
+    package.path = app_dir .. "/.rocks/share/tarantool/?.lua;" .. package.path
+    package.path = app_dir .. "/.rocks/share/tarantool/?/init.lua;" .. package.path
+    package.cpath = app_dir .. "/?.so;" .. package.cpath
+    package.cpath = app_dir .. "/?.dylib;" .. package.cpath
+    package.cpath = app_dir .. "/.rocks/lib/tarantool/?.so;" .. package.cpath
+    package.cpath = app_dir .. "/.rocks/lib/tarantool/?.dylib;" .. package.cpath
 end
 
-require 'strict'.on()
-local log = require('log')
-fiber = require 'fiber'
+require "strict".on()
+local log = require("log")
+fiber = require "fiber"
 
-local address = os.getenv("TARANTOOL_ADDRESS") or '0.0.0.0'
+local address = os.getenv("TARANTOOL_ADDRESS") or "0.0.0.0"
 local port = os.getenv("TARANTOOL_PORT") or 3301
-local workdir = os.getenv("TARANTOOL_WORKDIR") or "tmp/standalone_"..port
+local workdir = os.getenv("TARANTOOL_WORKDIR") or "tmp/standalone_" .. port
 
-local fio = require('fio')
+local fio = require("fio")
 fio.mkdir(workdir)
 
-box.cfg{
-    listen = address..':'..port,
-    wal_mode = 'none',
+box.cfg {
+    listen = address .. ":" .. port,
+    wal_mode = "none",
     wal_dir = workdir, -- though WAL not used here by default, see above.
     memtx_dir = workdir,
     readahead = 10 * 1024 * 1024,
-    net_msg_max = 10 * 1024,
+    net_msg_max = 10 * 1024
 }
-box.schema.user.grant('guest', 'super', nil, nil, { if_not_exists = true })
+box.schema.user.grant("guest", "super", nil, nil, {if_not_exists = true})
 
-local centrifuge = require 'centrifuge'
+local centrifuge = require "centrifuge"
 
-centrifuge.init({is_master=true})
+centrifuge.init({is_master = true})
 
 if not fiber.self().storage.console then
-    require'console'.start()
+    require "console".start()
     os.exit()
 end
