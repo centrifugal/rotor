846c2c60f6c00fd8f18d0229ddd56eedacce0319
Alexander Emelin <frvzmb@gmail.com>
2021-05-22 02:42:51 +0300

    format tests

--- test/basic_test.lua
+++ test/basic_test.lua
@@ -1,108 +1,147 @@
-local fio = require('fio')
-local log = require('log')
-local netbox = require('net.box')
-local t = require('luatest')
+local fio = require("fio")
+local log = require("log")
+local netbox = require("net.box")
+local t = require("luatest")
 
-local g = t.group('centrifuge')
+local g = t.group("centrifuge")
 
-local helpers = require('cartridge.test-helpers')
+local helpers = require("cartridge.test-helpers")
 
-g.before_all(function()
+g.before_all(
+    function()
         local tempdir = fio.tempdir()
 
-        local cluster = helpers.Cluster:new({
+        local cluster =
+            helpers.Cluster:new(
+            {
                 datadir = tempdir,
-                server_command = 'init.lua',
+                server_command = "init.lua",
                 use_vshard = false,
-                replicasets = {{
-                        roles = {'centrifuge'},
-                        servers = {{
-                                alias = 'storage1',
-                               }}},
+                replicasets = {
                     {
-                        roles = {'centrifuge'},
-                        servers = {{
-                                alias = 'storage2',
-                                   },{
-                                alias = 'storage2',
-                    }}},
-                },
-        })
+                        roles = {"centrifuge"},
+                        servers = {
+                            {
+                                alias = "storage1"
+                            }
+                        }
+                    },
+                    {
+                        roles = {"centrifuge"},
+                        servers = {
+                            {
+                                alias = "storage2"
+                            },
+                            {
+                                alias = "storage2"
+                            }
+                        }
+                    }
+                }
+            }
+        )
         cluster:start()
         g.cluster = cluster
-end)
+    end
+)
 
-g.after_all(function()
+g.after_all(
+    function()
         g.cluster:stop()
-end)
+    end
+)
 
 function g.test_centrifuge()
     local cluster = g.cluster
-    local server = cluster:server('storage1')
+    local server = cluster:server("storage1")
 
-    local rc, err = server.net_box:call('centrifuge.add_presence', {'hellochannel', 160, 'clientid',
-                                                                    'userid', 'info', 'chaninfo'})
+    local rc, err =
+        server.net_box:call(
+        "centrifuge.add_presence",
+        {
+            "hellochannel",
+            160,
+            "clientid",
+            "userid",
+            "info",
+            "chaninfo"
+        }
+    )
     t.assert_equals(err, nil, err)
 
-    local rc, err = server.net_box:call('centrifuge.presence', {'hellochannel'})
+    local rc, err = server.net_box:call("centrifuge.presence", {"hellochannel"})
     t.assert_not_equals(rc, nil, err)
     t.assert_equals(err, nil, err)
 
     -- subscribe(id, channels)
-    local rc, err = server.net_box:call('centrifuge.subscribe', {1, {'hellochannel'}})
+    local rc, err = server.net_box:call("centrifuge.subscribe", {1, {"hellochannel"}})
     t.assert_equals(err, nil, err)
 
-    local rc, err = server.net_box:call('centrifuge.subscribe', {1, {'hellochannelasdfasdf'}})
+    local rc, err = server.net_box:call("centrifuge.subscribe", {1, {"hellochannelasdfasdf"}})
     t.assert_equals(err, nil, err)
 
-    local rc, tm = t.assert_error(server.net_box.call, server.net_box, 'centrifuge.publish', {'msgtype', 'hellochannel', 'Data for message', 'some info', 10, 10, 10})
+    local rc, tm =
+        t.assert_error(
+        server.net_box.call,
+        server.net_box,
+        "centrifuge.publish",
+        {"msgtype", "hellochannel", "Data for message", "some info", 10, 10, 10}
+    )
 
-    local rc, err = server.net_box:call('centrifuge.get_messages', {1, true, 1})
+    local rc, err = server.net_box:call("centrifuge.get_messages", {1, true, 1})
     t.assert_equals(err, nil, err)
 
-    local rc, tm = server.net_box:call('centrifuge.publish', {'msgtype', 'hellochannel', 'Data for message', 'some info', 10, 10, 10})
+    local rc, tm =
+        server.net_box:call(
+        "centrifuge.publish",
+        {"msgtype", "hellochannel", "Data for message", "some info", 10, 10, 10}
+    )
     t.assert_not_equals(rc, nil)
     t.assert_not_equals(tm, nil)
 
-    local rc, err = server.net_box:call('centrifuge.get_messages', {1, true, 1})
+    local rc, err = server.net_box:call("centrifuge.get_messages", {1, true, 1})
     t.assert_not_equals(rc, nil, err)
     t.assert_equals(err, nil, err)
 
-
-    local rc, tm = server.net_box:call('centrifuge.publish', {'msgtype', 'hellochannel', 'Data2 for message', 'some info', 10, 10, 10})
+    local rc, tm =
+        server.net_box:call(
+        "centrifuge.publish",
+        {"msgtype", "hellochannel", "Data2 for message", "some info", 10, 10, 10}
+    )
     t.assert_not_equals(rc, nil)
     t.assert_not_equals(tm, nil)
 
-    local rc, tm = server.net_box:call('centrifuge.history', {'hellochannel', 0, 10, true, 10})
+    local rc, tm = server.net_box:call("centrifuge.history", {"hellochannel", 0, 10, true, 10})
     t.assert_not_equals(rc, nil, rc)
     t.assert_not_equals(tm, nil, tm)
 
-    local rc, err = server.net_box:call('centrifuge.history', {'hellochannel', 0, 10, true, 10})
+    local rc, err = server.net_box:call("centrifuge.history", {"hellochannel", 0, 10, true, 10})
     t.assert_not_equals(rc, nil, rc)
     t.assert_not_equals(tm, nil, tm)
 
-    local rc, err = server.net_box:call('centrifuge.remove_history', {'hellochannel'})
+    local rc, err = server.net_box:call("centrifuge.remove_history", {"hellochannel"})
     t.assert_equals(rc, nil, rc)
     t.assert_equals(err, nil, err)
 
-    local rc, err = server.net_box:call('centrifuge.unsubscribe', {1, {'hellochannel'}})
+    local rc, err = server.net_box:call("centrifuge.unsubscribe", {1, {"hellochannel"}})
     t.assert_equals(err, nil, err)
 
-    local rc, err = server.net_box:call('centrifuge.unsubscribe', {1, {'hellochannelasdfasdf'}})
+    local rc, err = server.net_box:call("centrifuge.unsubscribe", {1, {"hellochannelasdfasdf"}})
     t.assert_equals(err, nil, err)
 
-    local rc, err = server.net_box:call('centrifuge.add_presence', {'hellochannel', 10, 'client id', 'user id', 'connection info', 'channel info'})
+    local rc, err =
+        server.net_box:call(
+        "centrifuge.add_presence",
+        {"hellochannel", 10, "client id", "user id", "connection info", "channel info"}
+    )
     t.assert_equals(rc, nil, rc)
     t.assert_equals(err, nil, err)
 
-    local rc, err = server.net_box:call('centrifuge.presence', {'hellochannel'})
+    local rc, err = server.net_box:call("centrifuge.presence", {"hellochannel"})
     t.assert_not_equals(rc, nil, rc)
     t.assert_equals(err, nil, err)
 
-    local rc, err = server.net_box:call('centrifuge.remove_presence', {'hellochannel', 'client id'})
+    local rc, err = server.net_box:call("centrifuge.remove_presence", {"hellochannel", "client id"})
     t.assert_equals(rc, nil, rc)
     t.assert_equals(err, nil, err)
-    
-    -- history
-    -- remove history
 end
